e66bd0e9ac57270058cd206e836ca9e98e4e6ecf
허주영 <wndudpower@gmail.com>
2023-12-14 19:59:36 +0900

    update

---
+++ src/main/kotlin/string/LongestCommonPrefix.kt
@@ -0,0 +1,32 @@
+package string
+
+
+fun main() {
+    val a = longestCommonPrefix(arrayOf("a"))
+    println(a)
+}
+
+
+fun longestCommonPrefix(strs: Array<String>): String {
+    var i = 0
+    val ii = strs.minOf { it.length }
+
+    while (i < ii) {
+        val ch = strs[0].getOrNull(i)
+
+        val map = strs.map {
+            it[i]
+        }.count { it == ch } == strs.size
+
+        if(map){
+            i++
+        }else{
+            if(i == 0 ){
+                return ""
+            }
+            break
+        }
+    }
+
+    return strs[0].substring(0, i)
+}
