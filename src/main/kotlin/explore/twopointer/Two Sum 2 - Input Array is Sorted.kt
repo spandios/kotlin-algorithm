62a3f560d058581e663b4e1280163053837bf7a2
허주영 <wndudpower@gmail.com>
2024-01-21 20:36:59 +0900

    feature: two sum 2

---
+++ src/main/kotlin/explore/twopointer/Two Sum 2 - Input Array is Sorted.kt
@@ -0,0 +1,32 @@
+package src.main.kotlin.explore.twopointer
+
+fun main() {
+    println(twoSum(intArrayOf(2, 7, 11, 15), 9).contentToString())
+}
+
+fun twoSum(
+    numbers: IntArray,
+    target: Int,
+): IntArray {
+    var left = 0
+    var right = numbers.size - 1
+    val intArray = IntArray(2)
+
+    while (true) {
+        val s = numbers[left] + numbers[right]
+        if (s == target) {
+            intArray[0] = left + 1
+            intArray[1] = right + 1
+            break
+        }
+        if (s > target) {
+            right--
+        } else {
+            left++
+        }
+    }
+
+    return intArray
+}
+
+// -5 -1 0
