4f99c328c5bfa01452d18b6d9b54939079ab035f
spandios <wndudpower@gmail.com>
2024-03-21 08:53:17 +0900

    add n2

--- src/main/kotlin/array/BestTimeToBuyAndSellStock.kt
+++ src/main/kotlin/array/BestTimeToBuyAndSellStock.kt
@@ -5,6 +5,18 @@ fun main() {
     println(maxProfit(intArrayOf(7, 1, 5, 3, 6, 4)))
 }
 
+// fun maxProfit(prices: IntArray): Int {
+//    var profit = 0
+//    for (i in prices.indices) {
+//        val buy = prices[i]
+//        for (j in i + 1..<prices.size) {
+//            val sell = prices[j]
+//            profit = (sell - buy).coerceAtLeast(profit)
+//        }
+//    }
+//    return profit
+// }
+
 fun maxProfit(prices: IntArray): Int {
     var profit = 0
     var minPrice = -1
